a7e870adcb24fb46a8a89c174b640dfe97d5cca1
olivertribe <oliver.lh.tribe@gmail.com>
2022-07-15 20:04:30 +1000

    Handle potions better

--- src/LoopHandler/loops/PotionLoop.ts
+++ src/LoopHandler/loops/PotionLoop.ts
@@ -17,8 +17,8 @@ export default class PotionLoop extends Loop {
 
   private async usePotionIfLow (): Promise<void> {
     const character = this.bot.character
-    const MPThreshold = character.max_mp < 500 ? character.max_mp : character.max_mp - 500
-    const HPThreshold = character.max_hp < 500 ? character.max_hp : character.max_hp - 500
+    const MPThreshold = character.max_mp < 1000 ? character.max_mp : character.max_mp - 500
+    const HPThreshold = character.max_hp < 1000 ? character.max_hp : character.max_hp - 500
 
     const mpot0Loc = character.locateItem('mpot0', character.items)
     const mpot1Loc = character.locateItem('mpot1', character.items)
@@ -37,11 +37,12 @@ export default class PotionLoop extends Loop {
       await this.useHP(hpot)
       await this.bot.wait(this.timeout)
     }
+
     if (character.mp < MPThreshold) await this.useMP(mpot)
   }
 
   private async useHP (hpot?: 'hpot0' | 'hpot1'): Promise<void> {
-    if (hpot === undefined || this.bot.character.max_mp < 1000) {
+    if (hpot === undefined) {
       if (!this.bot.character.canUse('regen_hp')) return
       return await this.bot.character.regenHP()
     }
@@ -53,7 +54,7 @@ export default class PotionLoop extends Loop {
   }
 
   private async useMP (mpot?: 'mpot0' | 'mpot1'): Promise<void> {
-    if (mpot === undefined || this.bot.character.max_mp < 1000) {
+    if (mpot === undefined) {
       if (!this.bot.character.canUse('regen_mp')) return
       return await this.bot.character.regenMP()
     }
