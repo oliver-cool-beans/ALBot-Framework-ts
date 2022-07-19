abc58b776c0ada2f3275021c2cc854656911fcf4
olivertribe <oliver.lh.tribe@gmail.com>
2022-07-20 01:05:34 +1000

    Add attackTankTarget to strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,12 +1,14 @@
 import KiteTarget from './KiteTarget.js'
 import SafeAttack from './SafeAttack.js'
+import AttackTankTarget from './AttackTankTarget.js'
 
 export const moveStrategies = {
   kiteTarget: KiteTarget
 }
 
 export const attackStrategies = {
-  safeAttack: SafeAttack
+  safeAttack: SafeAttack,
+  attackTankTarget: AttackTankTarget
 }
 
 export const defenceStrategies = {
