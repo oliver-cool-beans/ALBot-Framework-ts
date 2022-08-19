23db7f074c4bb6ce63ff1f81e3bd18b217805551
olivertribe <oliver.lh.tribe@gmail.com>
2022-08-19 22:10:59 +1000

    Add mana checks to rouge attack strat

--- src/Bot/rogue/strategies/attack.ts
+++ src/Bot/rogue/strategies/attack.ts
@@ -4,8 +4,8 @@ export async function attack (bot: Bot, targetData) {
   const { character } = bot
   if (!targetData?.id) return
 
-  console.log(character.canUse('mentalburst'), targetData.hp <= character.attack * 0.6)
-  if (character.canUse('mentalburst') && targetData.hp <= character.attack * 0.6 && bot.isLowMp(70)) {
+  console.log(character.canUse('mentalburst'), targetData.hp <= character.attack)
+  if (character.canUse('mentalburst') && targetData.hp <= character.attack && bot.isLowMp(70)) {
     await character.mentalBurst(targetData.id).catch((error) => {
       bot.logger.error(`${bot.name} failed to mentalburst - ${error}`)
     })
@@ -17,7 +17,7 @@ export async function attack (bot: Bot, targetData) {
     })
   }
 
-  if (character.canUse('quickstab')) {
+  if (character.canUse('quickstab') && !bot.isLowMp(50)) {
     await character.quickStab(targetData.id).catch((error) => {
       bot.logger.error(`${bot.name} failed to quickstab - ${error}`)
     })
